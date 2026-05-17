be0262ebb22a4ae8c120d9b8bf9e46420bc89aef
Andrew <kezin636@gmail.com>
2026-05-17 18:26:40 +0300

    chore: update installed packages and remove unnecessary desktop entries

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux fastfetch
+dnf5 install -y btop fastfetch
 
+# remove unwanted kde games
+dnf5 remove -y kmahjongg kmines
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -30,3 +32,6 @@ sed -i 's/^ID_LIKE=.*/ID_LIKE="fedora"/' /usr/lib/os-release
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Project GDL"/' /usr/lib/os-release
 sed -i 's|^HOME_URL=.*|HOME_URL="https://github.com/ProjectGDL"|' /usr/lib/os-release
 sed -i 's|^BUG_REPORT_URL=.*|BUG_REPORT_URL="https://github.com/ProjectGDL/issues"|' /usr/lib/os-release
+
+rm /usr/share/applications/htop.desktop
+rm /usr/share/applications/nvtop.desktop
